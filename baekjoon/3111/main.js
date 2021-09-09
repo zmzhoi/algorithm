b6b9c2c920b80c1c5a5170bebcae63bd4d936621
zmzhoi <zmzhoi@gmail.com>
2021-09-09 15:41:54 +0900

    feat: baekjoon/3111

---
+++ baekjoon/3111/main.js
@@ -0,0 +1,38 @@
+function reverse(str) {
+  let reversed = "";
+
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+  }
+
+  return reversed;
+}
+
+function main(A, T) {
+  const WORD = A;
+  const WORD_REVERSE = reverse(A);
+
+  let odd = true;
+  let text = T;
+  let found;
+  do {
+    let index;
+    if (odd) {
+      index = text.indexOf(WORD);
+    } else {
+      index = text.lastIndexOf(WORD);
+    }
+
+    found = index > -1;
+    if (found) {
+      text =
+        text.slice(0, index) + text.slice(index + WORD.length, text.length);
+    }
+
+    odd = !odd;
+  } while (found);
+
+  console.log("RESULT: ", text);
+}
+
+main("banana", "babananananadeda");
