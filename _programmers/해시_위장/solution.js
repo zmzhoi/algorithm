d8686ba39e5e988e10d9cd5cf740c41f8c62527b
최종명 <zmzhoi@gmail.com>
2021-10-19 16:41:17 +0900

    add: programmers/해시_위장

---
+++ _programmers/해시_위장/solution.js
@@ -0,0 +1,24 @@
+function solution(clothes) {
+  const map = {};
+  clothes.map((clothe) => {
+    const [name, type] = clothe;
+    if (!map[type]) {
+      map[type] = [name];
+    } else {
+      map[type].push(name);
+    }
+  });
+
+  return (
+    Object.keys(map).reduce((acc, key) => acc * (map[key].length + 1), 1) - 1
+  );
+}
+
+/*
+- -
+- - -
+- -
+
+2^3 = 8
+2^1 = 2
+*/
